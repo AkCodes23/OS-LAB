a32737066bda27fac850c5636cf906600f4a49e1
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2025-01-31 09:52:08 +0530

    Update Q2.c

--- Lab 5/Q2.c
+++ Lab 5/Q2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>  // Include this header file
+#include <sys/wait.h>  
 
 void sortStrings(char *arr[], int n) {
     char *temp;
