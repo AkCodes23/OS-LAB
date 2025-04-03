f36486a5e6128667d7639bb3756441bcd9b57de1
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2025-04-04 00:23:35 +0530

    Update Q2.c

--- Lab 7/Q2.c
+++ Lab 7/Q2.c
@@ -1,58 +1,72 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <unistd.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t wrt = PTHREAD_MUTEX_INITIALIZER;
-int read_count = 0;
-int data = 0; 
+sem_t resource;       // Semaphore to control access to the resource
+pthread_mutex_t mutex; // Mutex to protect read_count
+int read_count = 0;    // Number of readers accessing the resource
 
-void* reader(void* arg) {
-    while(1) {
-        pthread_mutex_lock(&mutex);
-        read_count++;
-        if (read_count == 1) {
-            pthread_mutex_lock(&wrt);
-        }
-        pthread_mutex_unlock(&mutex);
-
-        // Reading section
-        printf("Reader read data: %d\n", data);
-
-        pthread_mutex_lock(&mutex);
-        read_count--;
-        if (read_count == 0) {
-            pthread_mutex_unlock(&wrt);
-        }
-        pthread_mutex_unlock(&mutex);
+void *reader(void *arg) {
+    int reader_id = *((int *)arg);
+    pthread_mutex_lock(&mutex);
+    read_count++;
+    if (read_count == 1) {
+        sem_wait(&resource); // First reader locks the resource
     }
+    pthread_mutex_unlock(&mutex);
+
+    // Reading section
+    printf("Reader %d is reading the resource.\n", reader_id);
+    sleep(1); // Simulate reading time
+
+    pthread_mutex_lock(&mutex);
+    read_count--;
+    if (read_count == 0) {
+        sem_post(&resource); // Last reader unlocks the resource
+    }
+    pthread_mutex_unlock(&mutex);
+    return NULL;
 }
 
-void* writer(void* arg) {
-    while(1) {
-        pthread_mutex_lock(&wrt);
+void *writer(void *arg) {
+    int writer_id = *((int *)arg);
+    sem_wait(&resource); // Writer locks the resource
 
-        // Writing section
-        data++;
-        printf("Writer wrote data: %d\n", data);
+    // Writing section
+    printf("Writer %d is writing to the resource.\n", writer_id);
+    sleep(1); // Simulate writing time
 
-        pthread_mutex_unlock(&wrt);
-    }
+    sem_post(&resource); // Writer unlocks the resource
+    return NULL;
 }
 
 int main() {
-    pthread_t r1, r2, w1;
+    pthread_t readers[5], writers[2];
+    int reader_ids[5] = {1, 2, 3, 4, 5};
+    int writer_ids[2] = {1, 2};
 
-    pthread_create(&r1, NULL, reader, NULL);
-    pthread_create(&r2, NULL, reader, NULL);
-    pthread_create(&w1, NULL, writer, NULL);
+    sem_init(&resource, 0, 1);
+    pthread_mutex_init(&mutex, NULL);
 
-    pthread_join(r1, NULL);
-    pthread_join(r2, NULL);
-    pthread_join(w1, NULL);
+    // Create reader and writer threads
+    for (int i = 0; i < 5; i++) {
+        pthread_create(&readers[i], NULL, reader, &reader_ids[i]);
+    }
+    for (int i = 0; i < 2; i++) {
+        pthread_create(&writers[i], NULL, writer, &writer_ids[i]);
+    }
+
+    // Join reader and writer threads
+    for (int i = 0; i < 5; i++) {
+        pthread_join(readers[i], NULL);
+    }
+    for (int i = 0; i < 2; i++) {
+        pthread_join(writers[i], NULL);
+    }
+
+    sem_destroy(&resource);
+    pthread_mutex_destroy(&mutex);
 
     return 0;
 }
-
-//gcc -o Q2 Q2.c -lpthread -lrt
-//.Q2
